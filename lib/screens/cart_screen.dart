bfa4910f9e9960b133841ba8662277afeda64378
Berkay Sarmasoğlu <berkaysarmasoglu2005@gmail.com>
2026-04-16 21:12:29 +0300

    Cart screen skeleton created

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart', style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: const Center(
+        child: Text(
+          'Your cart is empty',
+          style: TextStyle(fontSize: 18, color: Colors.grey),
+        ),
+      ),
+    );
+  }
+}
