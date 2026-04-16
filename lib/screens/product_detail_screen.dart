5dfab34a2ec69eea813374a5aec1ebb0e69cce48
Berkay Sarmasoğlu <berkaysarmasoglu2005@gmail.com>
2026-04-16 21:09:27 +0300

    product detail screen skeleton created

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
 
+class ProductDetailScreen extends StatelessWidget {
+  const ProductDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ürün Detay', style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ), // Otomatik geri butonunun rengi
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Ürün Görseli ve Detayları Buraya Gelecek'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Manuel olarak geri dönmek istersen Navigator.pop kullanılır
+                Navigator.pop(context);
+              },
+              child: const Text('Geri Dön'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
